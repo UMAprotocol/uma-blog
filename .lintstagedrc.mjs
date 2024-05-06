b4537b6f100718fa7a126c2eb67982610f830fcf
Gerhard Steenkamp <gerhard@umaproject.org>
2024-05-06 16:15:21 +0200

    add pre-commit hooks

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,13 @@
+import path from "path";
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+export default {
+  "*.{js,jsx,ts,tsx}": [
+    buildEslintCommand,
+    "prettier --write --config ./prettier.config.mjs",
+  ],
+};
