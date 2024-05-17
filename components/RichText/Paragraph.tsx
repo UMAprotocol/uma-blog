7ef01aa8f3f5decd2a6b92b762206f5fd3ccc629
Gerhard Steenkamp <gerhard@umaproject.org>
2024-05-17 16:37:48 +0200

    better select colors

--- components/RichText/Paragraph.tsx
+++ components/RichText/Paragraph.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function Paragraph({ children }: PropsWithChildren) {
   return (
-    <p className="text-md smoo text-text/75 tracking-wider font-light">
+    <p className="text-lg leading-6 text-text/75 tracking-wider font-light">
       {children}
     </p>
   );
