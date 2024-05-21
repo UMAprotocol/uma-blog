b3e1565f5d7a578add8bd988083727cb37d6a755
Gerhard Steenkamp <gerhard@umaproject.org>
2024-05-21 17:28:11 +0200

    handle existing record

--- lib/mailchimp.ts
+++ lib/mailchimp.ts
@@ -7,6 +7,13 @@ const userSchema = z.object({
   email: z.string().email(),
 });
 
+type MailchimpResponse = {
+  id: string; // hash of user email
+  title: string; // "Member Exists" if already subscribed
+  status: number; // http code
+  detail: string; // description
+};
+
 export async function addSubscriber(data: FormData) {
   const parsed = userSchema.safeParse({
     email: data.get("subscriberEmail"),
@@ -17,6 +24,8 @@ export async function addSubscriber(data: FormData) {
   }
 
   const { email } = parsed.data;
+
+  // https://mailchimp.com/developer/marketing/api/list-members/add-member-to-list/
 
   const url = `https://${env.MAILCHIMP_SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${env.MAILCHIMP_LIST_ID}/members?skip_merge_validation=true`;
   const encodedApiKey = Buffer.from(`key:${env.MAILCHIMP_API_KEY}`).toString(
@@ -35,13 +44,20 @@ export async function addSubscriber(data: FormData) {
     }),
   });
 
-  const responseData = (await res.json()) as Record<"id", string>;
+  const responseData = (await res.json()) as MailchimpResponse;
 
-  console.log(responseData);
+  // instead of attempting to create a record where one could already exists, we could check if the record exists first.
+  // https://mailchimp.com/developer/marketing/api/list-members/get-member-info/
+  // you would need to hash (MD5) the email
+
+  // handle existing record
+  if (responseData.title.toLowerCase().includes("member exists")) {
+    return true;
+  }
 
   if (!responseData.id) {
     throw new Error("Failed to add to list");
   }
-
+  // handle new record
   return true;
 }
