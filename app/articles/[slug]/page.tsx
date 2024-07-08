8954c3fec85d414474bec13ad984438339fe54e5
Gerhard Steenkamp <gerhard@umaproject.org>
2024-07-08 12:51:14 +0200

    feat: search via tag

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -157,7 +157,9 @@ export default async function BlogPage({ params: { slug } }: Props) {
 
               <div className="flex flex-wrap gap-2 items-center justify-start">
                 {post.fields.tags.map((tag) => (
-                  <Badge key={tag}>{tag}</Badge>
+                  <Badge href={`/?search=${tag}`} key={tag.toLowerCase()}>
+                    {tag}
+                  </Badge>
                 ))}
               </div>
             </div>
