a3851702233a6aca540318c509018660b14adff4
Gerhard Steenkamp <gerhard@umaproject.org>
2024-07-24 13:21:45 +0200

    improve search/filter ux

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -27,59 +27,81 @@ export function Filter({ className }: FilterProps) {
   const {
     text,
     handleTextChange,
+    tag,
     productParam,
     handleProductChange,
     clearAll,
   } = useFilter();
+
+  // eslint-disable-next-line no-mixed-operators
+  const hasParams = (text || tag) ?? productParam ? true : false;
+
   return (
-    <div
-      className={cn("relative flex flex-col md:flex-row gap-2 py-4", className)}
-    >
-      <Input
-        aria-label="Full text search input field"
-        value={text}
-        onChange={(e) => {
-          handleTextChange(e.target.value);
-        }}
-        className="md:max-w-[320px]"
-        type="text"
-        placeholder="Search"
-      />
-      <Divider orientation="vertical" />
-      <Select
-        value={productParam}
-        key={productParam}
-        onValueChange={(prod) => {
-          handleProductChange(prod as UmaProducts);
-        }}
+    <div className="flex w-full flex-col gap-2 relative py-4">
+      <div
+        className={cn(
+          "relative flex-1 flex flex-col md:flex-row gap-2 ",
+          className,
+        )}
       >
-        <SelectTrigger
-          aria-label="Choose an UMA product to filter by."
-          className="w-full md:w-[180px] capitalize placeholder:font-light placeholder:text-text-secondary"
+        <Input
+          aria-label="Full text search input field"
+          value={text}
+          onChange={(e) => {
+            handleTextChange(e.target.value);
+          }}
+          className="md:max-w-[320px]"
+          type="text"
+          placeholder="Search"
+        />
+        <Divider orientation="vertical" />
+        <Select
+          value={productParam}
+          key={productParam}
+          onValueChange={(prod) => {
+            handleProductChange(prod as UmaProducts);
+          }}
         >
-          <SelectValue placeholder="Product" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Products</SelectLabel>
-            {products.map((product) => (
-              <SelectItem className="capitalize" key={product} value={product}>
-                {product}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Button
-        onClick={() => {
-          clearAll();
-        }}
-        className="flex uppercase text-text-secondary hover:text-text font-light items-center gap-4"
-        variant="outline"
-      >
-        clear
-        <Icon className="w-[1em] h-[1em] text-inherit" name="close-x" />
-      </Button>
+          <SelectTrigger
+            aria-label="Choose an UMA product to filter by."
+            className="w-full md:w-[180px] capitalize placeholder:font-light placeholder:text-text-secondary"
+          >
+            <SelectValue placeholder="Product" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Products</SelectLabel>
+              {products.map((product) => (
+                <SelectItem
+                  className="capitalize"
+                  key={product}
+                  value={product}
+                >
+                  {product}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        {hasParams && (
+          <Button
+            onClick={() => {
+              clearAll();
+            }}
+            className="flex uppercase text-text-secondary hover:text-text font-light items-center gap-4"
+            variant="outline"
+          >
+            clear
+            <Icon className="w-[1em] h-[1em] text-inherit" name="close-x" />
+          </Button>
+        )}
+      </div>
+      {tag && (
+        <div className="uppercase mt-2 text-xl text-text-secondary font-light tracking-wider ">
+          topic: <span className="text-text">{tag}</span>
+        </div>
+      )}
+
       <Divider
         className="absolute w-[4000px] left-[calc(50%-2000px)] bottom-0"
         orientation="horizontal"
