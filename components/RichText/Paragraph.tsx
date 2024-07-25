c57b403227181cdfa532c44b2672c2518df53ee4
Gerhard Steenkamp <gerhard@umaproject.org>
2024-07-25 12:21:20 +0200

    make font thicker

--- components/RichText/Paragraph.tsx
+++ components/RichText/Paragraph.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function Paragraph({ children }: PropsWithChildren) {
   return (
-    <p className="text-lg w-full leading-6 text-text/75 tracking-wider font-light">
+    <p className="text-lg w-full leading-6 text-text/75 tracking-widest font-normal">
       {children}
     </p>
   );
